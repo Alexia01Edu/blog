53614a4aca4ae5d292610a7bffab43c45bc8d749
Alexia01Edu <101723744+Alexia01Edu@users.noreply.github.com>
2022-08-26 09:21:11 -0300

    Update banco.sql

--- sql/banco.sql
+++ sql/banco.sql
@@ -1,44 +1,88 @@
-
 -- Drop banco de dados
--- drop database blog
+--drop database DoIn;
 
 -- Criação do banco de dados
-create database blog;
+create database DoIn;
 
 -- selecionar banco de dados
-use blog;
+use DoIn;
 
+-- Criação da tabela usuario
 create table usuario (
-    id int not null auto_increment,
-    nome varchar(50) not null,
-    email varchar(255) not null, 
-    senha varchar(60) not null,
-    data_criacao datetime not null default CURRENT_TIMESTAMP,
-    ativo tinyint not null default '0',
-    adm tinyint not null default '0',
-    primary key (id)
-);
+	usuarioID			int 		    not null auto_increment,
+    nome 		        varchar(100)	not null,
+    email	            varchar(30)		not null,
+    senha 		        varchar(100)	not null,
+    cpf 		        varchar(100)	not null,
+    dataNasc 		    date	        not null,
+    telefone	        int		        not null,
+    quantDoar           int             not null,
+    quantTrocar         int             not null,
+    fk_ranking          int             not null,
+    primary key (usuarioID)
+    );
+    
+    create table categoria (
+	categoriaID			int 		    not null auto_increment,
+    nome 		        varchar(100)	not null,
+    primary key (categoriaID)
+    );
 
-create table post (
-    id int not null auto_increment,
-    titulo varchar(255) not null,
-    texto text not null,
-    usuario_id int not null,
-    data_criacao datetime not null default current_timestamp,
-    data_postagem datetime not null,
-    primary key (id),
-    key fk_post_usuario_idx (usuario_id),
-    constraint fk_post_usuario foreign key (usuario_id) references usuario (id)
-);
-create table avaliacao (
-id int not null auto_increment,
-nota int not null,
-comentario varchar(255) not null,
-usuario_id int not null,
-post_id int not null,
-data_criacao datetime not null default current_timestamp,
-primary key (id),
-constraint fk_avaliacao_usuario foreign key (usuario_id) references usuario (id),
-constraint fk_avaliacao_post foreign key (post_id) references post (id)
-);
+    create table Produto (
+	produtoID			int 		    not null auto_increment,
+    descricao 		    varchar(100)	not null,
+    nome_prod	        varchar(30)		not null,
+    quant 		        int         	not null,
+    modoOperacao        varchar(10)     not null,
+    dataValidade        date            not null,
+    estado 		        varchar(100)	not null,
+    cidade              varchar(100)    not null,
+    fk_categoria        int             not null,
+    fk_usuario          int             not null,
+    primary key (produtoID),
+    foreign key (fk_usuario) references usuario (usuarioID) on delete cascade on update cascade,
+    foreign key (fk_categoria) references categoria (categoriaID) on delete cascade on update cascade
+    );
+
+
+    create table Imagem (
+	imagemID			int 		    not null auto_increment,
+    Imagem_arq          varchar(255)    not null,
+    dataImg 		    datetime not null default current_timestamp,
+    descricaoImg	    varchar(30)		null,
+    imagem_name         varchar(100)    not null,
+    primary key (imagemID)
+    );
+    
+
+
+    create table lista_desejos (
+	fk_usuario			int 		    not null,
+    fk_produto 		    int         	not null,
+    data                date            not null,
+    descricao           text            null,
+    publicar            varchar(10)     not null,
+    primary key (fk_usuario, fk_produto),
+    foreign key (fk_usuario) references usuario (usuarioID) on delete cascade on update cascade,
+    foreign key (fk_produto) references Produto (produtoID) on delete cascade on update cascade
+    );
+ 
+
+    create table conversa (
+	fk_usuario			int 		    not null,
+    fk_usuario2 		int         	not null,
+    dataChat 		    date         	not null,
+    texto               text            not null,
+    primary key (fk_usuario, fk_usuario2),
+    foreign key (fk_usuario) references usuario (usuarioID) on delete cascade on update cascade,
+    foreign key (fk_usuario2) references usuario (usuarioID) on delete cascade on update cascade
+    );
+    
+    create table possui (
+	fk_imagem			int 		    not null,
+    fk_produto 		    int         	not null,
+    primary key (fk_imagem, fk_produto),
+    foreign key (fk_imagem) references Imagem (imagemID) on delete cascade on update cascade,
+    foreign key (fk_produto) references Produto (produtoID) on delete cascade on update cascade
+    );
 
