48fa912eb8c97e4beea67fe6c87cb9528e6d6072
alexia <alexiaedu012004@gmail.com>
2022-09-02 11:09:39 -0300

    doin

--- index.php
+++ index.php
@@ -9,24 +9,20 @@
             <div class="row">
                 <div class="col-md-12">
                     <!-- Topo -->
-                    <?php
-                    include 'includes/topo.php';
-                    ?>
+                    <?php include 'includes/topo.php'; ?>
                 </div>
             </div>
             <div class="row" style="min-height: 500px;">
                 <div class="col-md-12">
                     <!-- Menu -->
-                    <?php
-                        include 'includes/menu.php';
-                    ?>
+                    <?php include 'includes/menu.php'; ?>
                 </div>
+
                 <div class="col-md-10" style="padding-top: 50px;">
                     <!-- Conteúdo -->
                     <h2>Página Inicial</h2>
-                    <?php
-                    include 'includes/busca.php';
-                    ?>
+                    <?php include 'includes/busca.php'; ?>
+                    
                     <?php
                         require_once 'includes/funcoes.php';
                         require_once 'core/conexao_mysql.php';
