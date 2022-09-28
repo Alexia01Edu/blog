5526ebe1521ef938fb167ba48d25f35389f2a4a2
alexia <alexiaedu012004@gmail.com>
2022-09-28 10:42:17 -0300

    doin

--- sql/sql_comandos.sql
+++ sql/sql_comandos.sql
@@ -1,4 +1,3 @@
 delete from blog.usuario where id=2;
 delete from blog.post where id=4;
-INSERT INTO blog.usuario(nome, email, senha, ativo,adm)values('Alexia', 'a@gmail.com', '123',1,1);
 INSERT INTO blog.post(titulo, texto, usuario_id, data_postagem) VALUES('Como fazer pão de mel?', 'compre pão de mel, pois os ingredientes estão caros', 1, '2020-02-23 11:12:36');
