36961604461b93c58fbfc7ff5a34e62f6ee35a4a
Alexia01Edu <101723744+Alexia01Edu@users.noreply.github.com>
2022-10-05 10:06:43 -0300

    Update valida_login.php

--- includes/valida_login.php
+++ includes/valida_login.php
@@ -3,7 +3,7 @@
     $_SESSION['url_retorno'] = $_SERVER['PHP_SELF'];
     //PHP_SELF: O nome do arquivo do script atualmente em execução, relativo à raiz do documento(caminho).
     if(!isset($_SESSION['login'])) {
-        //se login existir
+        //se login não existir
         header('Location: login_formulario.php');
         exit;
     }
